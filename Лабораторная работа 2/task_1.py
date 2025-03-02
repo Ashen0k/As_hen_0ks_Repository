9aef6eeeabdcf6b2cc3eb7a53bb1d09bdb780085
Ashen0k <alexander.sniatkov@gmail.com>
2025-03-02 21:40:18 +0300

    Лабораторная работа 2 Исправлено

--- Лабораторная работа 2/task_1.py
+++ Лабораторная работа 2/task_1.py
@@ -1,10 +1,11 @@
 BOOKS_DATABASE = [
     {
-        "iden": 1,
+        "id": 1,
         "name": "test_name_1",
         "pages": 200,
     },
-    {   "iden": 2,
+    {
+        "id": 2,
         "name": "test_name_2",
         "pages": 400,
     }
@@ -12,19 +13,19 @@ BOOKS_DATABASE = [
 
 # TODO написать класс Book
 class Book:
-    def __init__(self, name: str, iden: int, pages: int):
+    def __init__(self, name: str, id: int, pages: int):
         self.name = name
-        self.iden = iden
+        self.id = id
         self.pages = pages
     def __str__(self) -> str:
         return f'Книга "{self.name}"'
     def __repr__(self) -> str:
-        return f'Book(id_={self.iden}, name={self.name!r}, pages={self.pages})'
+        return f'Book(id_={self.id}, name={self.name!r}, pages={self.pages})'
 
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
-        Book(iden=book_dict["iden"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
         ]
     for book in list_books:
         print(book)  # проверяем метод __str__
